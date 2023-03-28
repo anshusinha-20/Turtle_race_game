211f99ff68324abed70df00f98ab2ac8a9151352
Anshu Sinha <anshujuly2@gmail.com>
2023-03-28 20:54:25 +0530

    Add a code to make a sketch on the screen using the turtle object

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 """Turtle and Screen class is imported from the turtle module"""
+import turtle
 from turtle import Turtle, Screen
 
+##### ----- #####
 """tim object is created"""
 tim = Turtle()
-
-"""function to move forward by 10 paces"""
-def timMoveForward():
-    tim.forward(10)
-
+tim.shape("turtle")
 
 """screen object is created"""
 screen = Screen()
@@ -15,8 +13,53 @@ screen = Screen()
 """a method which used to make the turtle screen listen to the keyboard and mouse events"""
 screen.listen()
 
-"""triggers an event on a keypress"""
-screen.onkey(key="space", fun=timMoveForward)
+##### ----- #####
 
-"""screen will exit on click"""
-screen.exitonclick()
+##### ----- #####
+
+# """function to move forward by 10 paces"""
+# def timMoveForward():
+#     tim.forward(10)
+#
+# """triggers an event on a keypress"""
+# screen.onkey(key="space", fun=timMoveForward)
+# screen.exitonclick()
+
+##### ----- #####
+
+##### ----- #####
+
+"""function to draw a sketch"""
+
+def timMakeSketch(x, y):
+    def moveForward():
+        tim.forward(10)
+
+    def moveBackward():
+        tim.backward(10)
+
+    def turnRight():
+        tim.right(5)
+
+    def turnLeft():
+        tim.left(5)
+
+    screen.onkey(key="w", fun=moveForward)
+    screen.onkey(key="s", fun=moveBackward)
+    screen.onkey(key="a", fun=turnLeft)
+    screen.onkey(key="d", fun=turnRight)
+    screen.onkey(key="c", fun=tim.clear)
+
+    screen.exitonclick()
+
+"""function called on click"""
+screen.onscreenclick(timMakeSketch, 1)
+turtle.mainloop()
+
+##### ----- #####
+
+
+
+
+
+
